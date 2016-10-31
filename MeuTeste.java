20a05d9be7d1c7aede305467ef53bceb9600a042
Welton Vaz <weltonvaz@gmail.com>
2016-10-31 17:47:01 -0200

    Trabalho - Alteração feitas em 31/10/2016

---
+++ MeuTeste.java
@@ -0,0 +1,9 @@
+public class MeuTeste {
+   public static void main(String[] args) {
+       System.out.println("****************************************");
+       System.out.println("******     Hello gafanhotos       ******");
+       System.out.println("****************************************");
+       System.out.println("** Que venha o curso de Java OO!!! *****");
+       System.out.println("****************************************");
+   }
+}
